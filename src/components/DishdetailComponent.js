0fddf2dbe7a21e9a89e1b4143d750d9352e8e573
Neeraj Patil <patilneeraj2212@gmail.com>
2020-10-31 14:17:24 +0530

    fetch using server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 import {
 	Card,
 	CardImg,
@@ -141,7 +142,7 @@ function RenderDish({ dish }) {
 		return (
 			<div className="col-12 col-md-5 m-1">
 				<Card>
-					<CardImg top src={dish.image} alt={dish.name} />
+					<CardImg top src={baseUrl + dish.image} alt={dish.name} />
 					<CardBody>
 						<CardTitle>{dish.name}</CardTitle>
 						<CardText>{dish.description}</CardText>
