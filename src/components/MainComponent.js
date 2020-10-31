ea1fc554920726e1a840bec1c9fed794f1bfb3d3
Neeraj Patil <patilneeraj2212@gmail.com>
2020-10-31 15:45:59 +0530

    React Animations

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,6 +6,7 @@ import Footer from './FooterComponent';
 import About from './AboutComponent';
 import Contact from './contactComponent';
 import Home from './HomeComponent';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import {
@@ -86,28 +87,37 @@ class Main extends Component {
 		return (
 			<div>
 				<Header />
-				<Switch>
-					<Route path="/home" component={HomePage} />
-					<Route
-						exact
-						path="/menu"
-						component={() => <Menu dishes={this.props.dishes} />}
-					/>
-					<Route path="/menu/:dishId" component={DishWithId}></Route>
-					<Route
-						exact
-						path="/contactus"
-						component={() => (
-							<Contact resetFeedbackForm={this.props.resetFeedbackForm} />
-						)}
-					/>
-					<Route
-						exact
-						path="/aboutus"
-						component={() => <About leaders={this.props.leaders} />}
-					/>
-					<Redirect to="/home" />
-				</Switch>
+				<TransitionGroup>
+					<CSSTransition
+						key={this.props.location.key}
+						classNames="page"
+						timeout={300}
+					>
+						<Switch>
+							<Route path="/home" component={HomePage} />
+							<Route
+								exact
+								path="/menu"
+								component={() => <Menu dishes={this.props.dishes} />}
+							/>
+							<Route path="/menu/:dishId" component={DishWithId}></Route>
+
+							<Route
+								exact
+								path="/aboutus"
+								component={() => <About leaders={this.props.leaders} />}
+							/>
+							<Route
+								exact
+								path="/contactus"
+								component={() => (
+									<Contact resetFeedbackForm={this.props.resetFeedbackForm} />
+								)}
+							/>
+							<Redirect to="/home" />
+						</Switch>
+					</CSSTransition>
+				</TransitionGroup>
 				<Footer />
 			</div>
 		);
