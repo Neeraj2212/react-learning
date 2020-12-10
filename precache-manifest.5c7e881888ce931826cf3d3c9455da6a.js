2509d102866ba3357dd21a5d16fd974af5649b4b
Neeraj Patil <patilneeraj2212@gmail.com>
2020-12-10 17:39:54 +0530

    Updates

---
+++ precache-manifest.5c7e881888ce931826cf3d3c9455da6a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2162c55a469466986cc5f3bd8b869c36",
+    "url": "/react-learning/index.html"
+  },
+  {
+    "revision": "a03e40d687780e6a35df",
+    "url": "/react-learning/static/css/2.8aef1c17.chunk.css"
+  },
+  {
+    "revision": "9cc5113beb582cf80e48",
+    "url": "/react-learning/static/css/main.e3ec33a5.chunk.css"
+  },
+  {
+    "revision": "a03e40d687780e6a35df",
+    "url": "/react-learning/static/js/2.e8334636.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-learning/static/js/2.e8334636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cc5113beb582cf80e48",
+    "url": "/react-learning/static/js/main.55943171.chunk.js"
+  },
+  {
+    "revision": "e1c16abeae793e256bca",
+    "url": "/react-learning/static/js/runtime-main.4a508b5a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-learning/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-learning/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-learning/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-learning/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-learning/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
